e0c5aea68743552f8c62f7847f1da58ef59addd6
Mirko Westermeier <mirko.westermeier@uni-muenster.de>
2025-03-27 10:26:23 +0100

    Add file storing

--- lib/T4/Storage.hs
+++ lib/T4/Storage.hs
@@ -11,3 +11,9 @@ loadDataFromDir :: FilePath -> IO [Clock]
 loadDataFromDir dir = do
   ymlFiles <- filter (".yml" `isSuffixOf`) <$> listDirectory dir
   sort <$> concatMapM decodeFileThrow ((dir </>) <$> ymlFiles)
+
+writeDataToDir :: FilePath -> [Clock] -> IO ()
+writeDataToDir dir clocks = do
+  forM_ (dayGroups clocks) $ \dayGroup -> do
+    let day = dateString (time $ head dayGroup)
+    encodeFile (dir </> day <.> "yml") dayGroup
--- test/T4/StorageSpec.hs
+++ test/T4/StorageSpec.hs
@@ -10,13 +10,23 @@ import System.IO.Temp
 
 spec :: Spec
 spec = do
+
+  let c1 = In   (simpleLocalTime 2001 2 3 4 5 6) "cat1" ["t1", "t2"]
+      c2 = Out  (simpleLocalTime 2001 2 3 4 5 7)
+      c3 = In   (simpleLocalTime 2001 2 4 4 5 6) "cat2" ["t2", "t3"]
   
   context "Loading data" $ do
-    let c1 = In   (simpleLocalTime 2001 2 3 4 5 6) "cat1" ["t1", "t2"]
-        c2 = Out  (simpleLocalTime 2001 2 3 4 5 7)
-        c3 = In   (simpleLocalTime 2001 2 4 4 5 6) "cat2" ["t2", "t3"]
     loaded <- runIO $ withSystemTempDirectory "t4" $ \tdir -> do
       encodeFile (tdir </> "2001-02-03" <.> "yml") [c2, c1]
       encodeFile (tdir </> "2001-02-04" <.> "yml") [c3]
       loadDataFromDir tdir
     it "Loaded correct (sorted) clock data" $ loaded `shouldBe` [c1, c2, c3]
+
+  context "Storing data" $ do
+    (cs1, cs2) <- runIO $ withSystemTempDirectory "t4" $ \tdir -> do
+      writeDataToDir tdir [c2, c1, c3]
+      cs1 <- decodeFileThrow (tdir </> "2001-02-03" <.> "yml")
+      cs2 <- decodeFileThrow (tdir </> "2001-02-04" <.> "yml")
+      return (cs1, cs2)
+    it "Correct clocks in first file"   $ cs1 `shouldBe` [c1, c2]
+    it "Correct clocks in second file"  $ cs2 `shouldBe` [c3]
