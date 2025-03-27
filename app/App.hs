034371a4ccd02132f7b1ad42c1eaad1cecfe0cb7
Mirko Westermeier <mirko.westermeier@uni-muenster.de>
2025-03-27 15:49:49 +0100

    Simple brick app architecture

--- app/App.hs
+++ app/App.hs
@@ -1,5 +1,38 @@
 module Main where
 
+import Brick
+import Graphics.Vty
+import T4.Data
+import T4.Storage
+import Lens.Micro.Platform
+import Control.Monad
+import Control.Monad.IO.Class
+
+data T4State = T4State
+  { _dir    :: FilePath
+  , _clocks :: [Clock]
+  } deriving Show
+makeLenses ''T4State
+
 main :: IO ()
 main = do
-  putStrLn "T4"
+  let clockDir  = "t4-data" -- TODO
+      initState = T4State clockDir []
+  void $ defaultMain t4App initState
+
+t4App :: App T4State () ()
+t4App = App
+  { appDraw         = drawT4
+  , appChooseCursor = neverShowCursor
+  , appHandleEvent  = resizeOrQuit
+  , appStartEvent   = initT4
+  , appAttrMap      = const $ attrMap defAttr []
+  }
+
+initT4 :: EventM () T4State ()
+initT4 = do
+  loadedClocks <- liftIO . loadDataFromDir =<< use dir
+  clocks .= loadedClocks
+
+drawT4 :: T4State -> [Widget ()]
+drawT4 state = [str (show $ state ^. clocks)]
