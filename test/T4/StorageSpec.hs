882ddc7431f0b0dbe570856e8c8741f42371c055
Mirko Westermeier <mirko.westermeier@uni-muenster.de>
2025-03-27 09:53:49 +0100

    Test sorting in simple loading test

--- test/T4/StorageSpec.hs
+++ test/T4/StorageSpec.hs
@@ -16,7 +16,7 @@ spec = do
         c2 = Out  (simpleLocalTime 2001 2 3 4 5 7)
         c3 = In   (simpleLocalTime 2001 2 4 4 5 6) "cat2" ["t2", "t3"]
     loaded <- runIO $ withSystemTempDirectory "t4" $ \tdir -> do
-      encodeFile (tdir </> "2001-02-03" <.> "yml") [c1, c2]
+      encodeFile (tdir </> "2001-02-03" <.> "yml") [c2, c1]
       encodeFile (tdir </> "2001-02-04" <.> "yml") [c3]
       loadDataFromDir tdir
-    it "Loaded correct clock data" $ loaded `shouldBe` [c1, c2, c3]
+    it "Loaded correct (sorted) clock data" $ loaded `shouldBe` [c1, c2, c3]
