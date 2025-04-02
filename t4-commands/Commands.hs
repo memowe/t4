ee5da411fe9638de14b54e4c102edfeac45a36c2
Mirko Westermeier <mirko.westermeier@uni-muenster.de>
2025-04-02 16:39:46 +0200

    Define command line argument parser for clocking in and out

--- t4-commands/Commands.hs
+++ t4-commands/Commands.hs
@@ -1,5 +1,45 @@
 module Main where
 
+import T4.Data
+import Options.Applicative
+
+data Command  = CmdIn { ccat  :: Category
+                      , ctags :: [Tag]
+                      }
+              | CmdOut
+              deriving Show
+
+inParser :: Parser Command
+inParser =
+  CmdIn <$> strOption (   long    "category"
+                      <>  short   'c'
+                      <>  metavar "CATEGORY"
+                      <>  value   ""
+                      <>  help    "Simple name of a category"
+                      )
+        <*> some
+              ( argument  str
+                          (   metavar "TAGS"
+                          <>  help "List of tags, separated by space"
+                          )
+              )
+
+outParser :: Parser Command
+outParser = pure CmdOut
+
+commandParser :: Parser Command
+commandParser = hsubparser
+  (   command "in"  (info inParser  (progDesc "Clocking in"))
+  <>  command "out" (info outParser (progDesc "Clocking out"))
+  )
+
+opts :: ParserInfo Command
+opts = info (commandParser <**> helper)
+  (   fullDesc
+  <>  progDesc  "Simple interface for clocking in and out"
+  <>  header    "t4 - terminal time tracking tool"
+  )
+
 main :: IO ()
 main = do
-  putStrLn "T4"
+  print =<< execParser opts
