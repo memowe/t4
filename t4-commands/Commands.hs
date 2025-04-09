7e2a95484c14da39ee6049660675fe3bc12f1d22
Mirko Westermeier <mirko.westermeier@uni-muenster.de>
2025-04-09 10:08:53 +0200

    Add report command

--- t4-commands/Commands.hs
+++ t4-commands/Commands.hs
@@ -4,7 +4,11 @@ import T4.Data
 import T4.Storage
 import Data.List
 import Data.Time
+import Data.Map (Map, (!))
+import qualified Data.Map as M
+import Control.Monad
 import Options.Applicative
+import T4.Report (categoryDurations, tagDurations)
 
 data Command  = CmdIn { ccat  :: Maybe Category
                       , ctags :: [Tag]
@@ -13,6 +17,9 @@ data Command  = CmdIn { ccat  :: Maybe Category
               | CmdStatus
               | CmdCats
               | CmdTags
+              | CmdReport { crepCat   :: Bool
+                          , crepTags  :: Bool
+                          }
 
 inParser :: Parser Command
 inParser =
@@ -42,6 +49,21 @@ catsParser = pure CmdCats
 tagsParser :: Parser Command
 tagsParser = pure CmdTags
 
+reportParser :: Parser Command
+reportParser = correct <$> catSwitch <*> tagSwitch
+  where catSwitch = switch
+                      (   long "categories"
+                      <>  short 'c'
+                      <>  help "Include categories in the report"
+                      )
+        tagSwitch = switch
+                      (   long "tags"
+                      <>  short 't'
+                      <>  help "Include tags in the report"
+                      )
+        correct False False = CmdReport True  True
+        correct c     t     = CmdReport c     t
+
 commandParser :: Parser Command
 commandParser = hsubparser
   (   command "in"          (info inParser      (progDesc "Clocking in"))
@@ -49,6 +71,7 @@ commandParser = hsubparser
   <>  command "status"      (info statusParser  (progDesc "Show current status"))
   <>  command "categories"  (info catsParser    (progDesc "List all categories"))
   <>  command "tags"        (info tagsParser    (progDesc "List all tags"))
+  <>  command "report"      (info reportParser  (progDesc "Report"))
   )
 
 opts :: ParserInfo Command
@@ -68,21 +91,37 @@ addClock clock = do
   writeDataToDir dd (clock : clocks)
 
 handle :: Command -> IO ()
-handle (CmdIn c ts) = do  cslt <- getCurrentSLT
-                          addClock $ In cslt c ts
-handle CmdOut       = do  cslt <- getCurrentSLT
-                          addClock $ Out cslt
-handle CmdStatus    = do  dd      <- getStorageDirectory
-                          clocks  <- loadDataFromDir dd
-                          putStrLn $ case clocks of
-                            [] -> "No clock data yet"
-                            cs -> summary $ last cs
-handle CmdCats      = do  dd      <- getStorageDirectory
-                          clocks  <- loadDataFromDir dd
-                          mapM_ putStrLn (sort $ allCategories clocks)
-handle CmdTags      = do  dd      <- getStorageDirectory
-                          clocks  <- loadDataFromDir dd
-                          mapM_ putStrLn (sort $ allTags clocks)
+handle (CmdIn c ts)           = do  cslt <- getCurrentSLT
+                                    addClock $ In cslt c ts
+handle CmdOut                 = do  cslt <- getCurrentSLT
+                                    addClock $ Out cslt
+handle CmdStatus              = do  dd      <- getStorageDirectory
+                                    clocks  <- loadDataFromDir dd
+                                    putStrLn $ case clocks of
+                                      [] -> "No clock data yet"
+                                      cs -> summary $ last cs
+handle CmdCats                = do  dd      <- getStorageDirectory
+                                    clocks  <- loadDataFromDir dd
+                                    mapM_ putStrLn (sort $ allCategories clocks)
+handle CmdTags                = do  dd      <- getStorageDirectory
+                                    clocks  <- loadDataFromDir dd
+                                    mapM_ putStrLn (sort $ allTags clocks)
+handle (CmdReport True  True) = do  dd      <- getStorageDirectory
+                                    clocks  <- loadDataFromDir dd
+                                    putStrLn "Categories"
+                                    showMap 2 (categoryDurations clocks)
+                                    putStrLn "Tags"
+                                    showMap 2 (tagDurations clocks)
+handle (CmdReport c     t)    = do  dd      <- getStorageDirectory
+                                    clocks  <- loadDataFromDir dd
+                                    when c $ showMap 0 $ categoryDurations  clocks
+                                    when t $ showMap 0 $ tagDurations       clocks
+
+showMap :: Show a => Int -> Map String a -> IO ()
+showMap indent m = do
+  forM_ (M.keys m) $ \k -> do
+    putStr    $ replicate indent ' '
+    putStrLn  $ k ++ ": " ++ show (m ! k)
 
 main :: IO ()
 main = do
